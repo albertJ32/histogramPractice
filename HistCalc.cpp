8f8a83547237849c77197a1a4826ae4ba582667a
albertJ32 <00b0d14c@opayq.com>
2015-07-20 14:42:46 +0100

    display single channel values/histogram

--- HistCalc.cpp
+++ HistCalc.cpp
@@ -11,60 +11,47 @@ using namespace cv;
  */
 int main( int argc, char** argv )
 {
-  Mat src, dst;
-
-  /// Load image
-  src = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-  if( !src.data )
-    { return -1; }
-
-  /// Separate the image in 3 places ( B, G and R )
-  vector<Mat> bgr_planes;
-  split( src, bgr_planes );
 
   /// Establish the number of bins
-  int histSize = 256;
+  int histSize = 20;
 
   /// Set the ranges ( for B,G,R) )
-  float range[] = { 0, 256 } ;
+  float range[] = { 0, 100 } ;
   const float* histRange = { range };
 
   bool uniform = true; bool accumulate = false;
 
-  Mat b_hist, g_hist, r_hist;
+  int h = 20, w = 20;
+  int total = (h*w)*100;
+  Mat test = Mat(h,w,CV_32FC1, Scalar(23));
 
- equalizeHist(bgr_planes[0],bgr_planes[0]);
- // equalizeHist(bgr_planes[1],bgr_planes[1]);
- // equalizeHist(bgr_planes[2],bgr_planes[2]);
+  // Add pixel variations
+  for(int i=0;i<168;i++)
+    test.at<float>(10,i) = 90;
+
+  test.at<float>(15,3) = 11;
 
   /// Compute the histograms:
-  calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-  // calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-  // calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+  Mat test1;
+  calcHist( &test, 1, 0, Mat(), test1, 1, &histSize, &histRange, uniform, accumulate );
 
   // // Draw the histograms for B, G and R
   int hist_w = 512; int hist_h = 400;
   int bin_w = cvRound( (double) hist_w/histSize );
 
- Mat histImage( hist_h, hist_w, CV_8UC1, Scalar( 0) );
+  Mat histImage( hist_h, hist_w, CV_8UC1, Scalar( 0) );
 
   /// Normalize the result to [ 0, histImage.rows ]
-  normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  // normalize(test1, test1, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
   // normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
   // normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
 
   /// Draw for each channel
   for( int i = 1; i < histSize; i++ )
   {
-      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-                       Scalar( 255, 0, 0), 2, 8, 0  );
-  //     line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-  //                      Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-  //                      Scalar( 0, 255, 0), 2, 8, 0  );
-  //     line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-  //                      Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-  //                      Scalar( 0, 0, 255), 2, 8, 0  );
+       rectangle( histImage, Point( bin_w*(i),  hist_h) ,
+                        Point( (bin_w*(i))+bin_w,  hist_h - test1.at<float>(i)),
+                        Scalar( 255, 255, 255),-1, 8);
   }
 
   /// Display
