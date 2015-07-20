9a3f46c4fb33eccea841c94466434e647bd91ce8
albertJ32 <00b0d14c@opayq.com>
2015-07-20 12:56:56 +0100

    initial commit

---
+++ build/CMakeFiles/HistCalc.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/james-tt/Dropbox/Term 2/Final project/OpenCv Programs/3 Image Processing/HistCalc/HistCalc.cpp" "/home/james-tt/Dropbox/Term 2/Final project/OpenCv Programs/3 Image Processing/HistCalc/build/CMakeFiles/HistCalc.dir/HistCalc.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/HistCalc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HistCalc.dir/HistCalc.cpp.o"
+  "HistCalc.pdb"
+  "HistCalc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HistCalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
