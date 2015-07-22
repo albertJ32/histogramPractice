57d99a29762283c6317061b4b3c35a2c15b77e70
albertJ32 <00b0d14c@opayq.com>
2015-07-22 15:04:23 +0100

    collecting two imgs from external camera, bug fixes

--- HistCalc.cpp
+++ HistCalc.cpp
@@ -71,20 +71,34 @@ Mat createHist(Mat& test1, Mat& histImage){
   return showHist(test, histImage, histSize);
 }
 
-void getImgs(Mat& img){
+void getImgs(Mat& img, Mat& img2){
   VideoCapture stream1(1);
   if(!stream1.isOpened()){
     cout << "cannot open video stream" << endl;
   }
 
+  int imgCount = 0;
+
+  cout << "\n\nEntering img collection loop. Press '1' and '2' to collect images. Press 'q' to leave loop.\n\n";
+
   while(true){
     Mat imgTmp;
     stream1.read(imgTmp);
     imshow("VideoStream", imgTmp);
+
     char key = waitKey(30);
-    if(key == 'q'){
+
+    if(key == '1'){
       cout << "this is the leypress" << key << endl;
       imgTmp.copyTo(img);
+      imgCount++;
+    } else if(key == '2'){
+      cout << "this is the leypress" << key << endl;
+      imgTmp.copyTo(img2);
+      imgCount++;
+    } else if(key == 'q' && (img.empty() || img2.empty())){
+      cout << "Please collect both image '1' and image '2'" << endl;
+    } else if(key == 'q'){
       break;
     }
   }
@@ -92,20 +106,23 @@ void getImgs(Mat& img){
 
 int main( int argc, char** argv )
 {
-  Mat savedPic;
+  Mat savedPic1, savedPic2;
 
-  getImgs(savedPic);
-  if(!savedPic.data){
+  getImgs(savedPic1, savedPic2);
+  if(savedPic1.empty() || savedPic2.empty()){
     cout << "savedPic does not have any data.." << endl;
     return -1;
   }
-  
-  // Equalise captured image
-  cvtColor(savedPic, savedPic, CV_BGR2GRAY);
-  equalizeHist(savedPic, savedPic);
+
+  // Equalise captured images
+  cvtColor(savedPic1, savedPic1, CV_BGR2GRAY);
+  equalizeHist(savedPic1, savedPic1);
+
+  cvtColor(savedPic2, savedPic2, CV_BGR2GRAY);
+  equalizeHist(savedPic2, savedPic2);
 
   // Calculate and return histogram representation
-  Mat histImage = createHist(savedPic, histImage);
+  Mat histImage = createHist(savedPic1, histImage);
 
   /// Display
   namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
